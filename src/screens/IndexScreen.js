bcf38168a0b65c7ba31dc62e46220bdf5ef16ce9
amin lahiai <aminlahiani@outlook.com>
2021-03-21 00:23:37 +0100

    Adding Context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+
+  const value = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
-    </View>
+      <Text>{value}</Text>
+      </View>
   );
 };
 
