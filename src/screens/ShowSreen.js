47f7775c43b2cd7043cdba8132f5fa4025f63b84
amin lahiai <aminlahiani@outlook.com>
2021-03-20 17:11:03 +0100

    add some screens

---
+++ src/screens/ShowSreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ShowSreen = () => {
+  return (
+    <View>
+      <Text>ShowSreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowSreen;
