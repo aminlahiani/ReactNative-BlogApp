babb4e68e41f25d6d9f0c32f098b0f0f5179180c
amin lahiai <aminlahiani@outlook.com>
2021-03-21 19:25:08 +0100

    Retrieving Single Posts

--- src/screens/ShowSreen.js
+++ src/screens/ShowSreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React  , { useContext}from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { useRoute } from '@react-navigation/native';
+import { Context } from '../context/BlogContext';
 const ShowSreen = () => {
+  const { state } = useContext(Context);
+ 
+  const route = useRoute();
+  const id = route.params.id
+  const blogPost = state.find(
+    blogPost => blogPost.id === id
+  );
+  console.log(id)
   return (
     <View>
-      <Text>ShowSreen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
